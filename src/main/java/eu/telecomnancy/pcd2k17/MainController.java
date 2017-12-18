3f17678060a5c05006d47a4e606833ef93234044
Quentin LEFEUVRE <quentin.lefeuvre@telecomnancy.eu>
2017-12-18 16:10:15 +0100

    Affichage projets de l'utilisateur connecté

--- src/main/java/eu/telecomnancy/pcd2k17/MainController.java
+++ src/main/java/eu/telecomnancy/pcd2k17/MainController.java
@@ -2,15 +2,18 @@ package eu.telecomnancy.pcd2k17;
 
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import javafx.scene.control.ScrollPane;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class MainController {
@@ -20,32 +23,13 @@ public class MainController {
   final static Logger log = LogManager.getLogger(MainController.class);
 
   @FXML
-  public void handleClickOk(ActionEvent event) {
+  public void handleClickOk(ActionEvent event) throws IOException {
     Stage stage = (Stage) button.getScene().getWindow();
     stage.close();
 
     log.debug("ok button was clicked!");
 
-    Stage window = new Stage();
-    window.setTitle("SchoolRoom - Project");
-
-    FXMLLoader loader = new FXMLLoader();
-    loader.setLocation(getClass().getResource("ProjectView.fxml"));
-    loader.setControllerFactory(iC-> new ProjectView());
-    Parent root = null;
-    try {
-      root = loader.load();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    window.setOnCloseRequest(event2 -> {
-      log.debug("terminating application.");
-      Platform.exit();
-    });
-
-    window.setScene(new Scene(root, 800, 600));
-    window.show();
+    new ProjectView("vQZL2K-1161fbxFAwLsS");
 
   }
 
--- src/main/java/eu/telecomnancy/pcd2k17/ProjectView.java
+++ src/main/java/eu/telecomnancy/pcd2k17/ProjectView.java
@@ -1,12 +1,20 @@
 package eu.telecomnancy.pcd2k17;
 
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
 import org.gitlab4j.api.models.Project;
@@ -16,17 +24,57 @@ import java.util.List;
 
 public class ProjectView {
 
+    @FXML
+    TitledPane t1;
+    @FXML
+    Accordion accordion= new Accordion();
 
-    public ProjectView() {
-        GitLabApi gitLab = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr", "vQZL2K-1161fbxFAwLsS");
+
+    final static Logger log = LogManager.getLogger(ProjectView.class);
+
+    public ProjectView(String privateToken) throws IOException {
+
+        GitLabApi gitLab = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr", privateToken);
+
         try {
             List<Project> list = gitLab.getProjectApi().getMemberProjects();
+            final TitledPane[] tps = new TitledPane[list.size()];
+            int i=0;
             for (Project p : list) {
+                tps[i]=new TitledPane(p.getName(), new Button("OK"));
                 System.out.println(p.getName());
                 System.out.println(p.getArchived());
             }
+            accordion.getPanes().addAll(tps);
+            accordion.setExpandedPane(tps[0]);
         } catch (GitLabApiException e) {
             e.printStackTrace();
         }
+
+        Stage window = new Stage();
+        window.setTitle("SchoolRoom - Project");
+        Scene scene = new Scene(new Group(), 800, 600);
+
+        /*
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("ProjectView.fxml"));
+
+        AnchorPane root = loader.load();
+        */
+
+        window.setOnCloseRequest(event2 -> {
+            log.debug("terminating application.");
+            Platform.exit();
+        });
+
+        //root.getChildren().add(accordion);
+
+        Group root = (Group)scene.getRoot();
+        root.getChildren().add(accordion);
+        window.setScene(scene);
+        //window.setScene(new Scene(root, 800, 600));
+        window.show();
+
     }
+
 }
