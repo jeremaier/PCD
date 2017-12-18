bfd22d8c236dda7e6ca03dd92ecf4c43430e1404
Jeremy <jeremy.sernit@free.fr>
2017-12-18 13:22:15 +0100

    Configuration view 0.1

--- src/main/java/eu/telecomnancy/pcd2k17/ConfigurationView.java
+++ src/main/java/eu/telecomnancy/pcd2k17/ConfigurationView.java
@@ -1,4 +1,35 @@
 package eu.telecomnancy.pcd2k17;
 
-public class ConfigurationView {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ConfigurationView extends Application {
+    final static Logger log = LogManager.getLogger(ConfigurationView.class);
+
+    public void startConfigurationView() {
+        log.debug("executing ConfigurationView method.");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("JFX Sample Application");
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("configurationView.fxml"));
+        Parent root = loader.load();
+
+        primaryStage.setOnCloseRequest(event -> {
+            log.debug("terminating application.");
+            Platform.exit();
+        });
+
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+    }
 }
