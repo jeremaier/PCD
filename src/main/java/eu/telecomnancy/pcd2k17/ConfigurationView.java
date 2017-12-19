b08bbafd6c85d01002441d3f59c4ace0d96ec4c1
Jeremy <jeremy.sernit@free.fr>
2017-12-19 08:08:46 +0100

    Configuration view 0.1

--- src/main/java/eu/telecomnancy/pcd2k17/ConfigurationView.java
+++ src/main/java/eu/telecomnancy/pcd2k17/ConfigurationView.java
@@ -1,35 +1,40 @@
 package eu.telecomnancy.pcd2k17;
 
-import javafx.application.Application;
-import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.gitlab4j.api.GitLabApi;
 
-public class ConfigurationView extends Application {
+import java.time.LocalDate;
+
+public class ConfigurationView {
+
     final static Logger log = LogManager.getLogger(ConfigurationView.class);
+    private static Project project;
+    private static GitLabApi gitLab;
 
-    public void startConfigurationView() {
+    public void startConfigurationView(GitLabApi gitLab/*, ProjectView projectView*/) {
+        this.gitLab = gitLab;
         log.debug("executing ConfigurationView method.");
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("JFX Sample Application");
 
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("configurationView.fxml"));
-        Parent root = loader.load();
+    }
 
-        primaryStage.setOnCloseRequest(event -> {
-            log.debug("terminating application.");
-            Platform.exit();
-        });
+    public static Project getProject() {
+        return project;
+    }
 
-        primaryStage.setScene(new Scene(root, 500, 500));
-        primaryStage.show();
+    public GitLabApi getGitLab() {
+        return this.gitLab;
+    }
+
+    public static void setProject(String title, String module, String nbMembers, LocalDate firstDate,LocalDate lastDate, String description) {
+        project = new Project(title, module, nbMembers, firstDate, lastDate, description);
     }
 }
--- src/main/java/eu/telecomnancy/pcd2k17/ConfigurationViewController.java
+++ src/main/java/eu/telecomnancy/pcd2k17/ConfigurationViewController.java
@@ -1,4 +1,104 @@
 package eu.telecomnancy.pcd2k17;
 
-public class ConfigurationViewController {
+import javafx.beans.property.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
+
+public class ConfigurationViewController implements Initializable {
+    final static Logger log = LogManager.getLogger(ConfigurationViewController.class);
+
+    @FXML
+    private AnchorPane controllerView;
+
+    @FXML
+    private Button AddFile;
+
+    @FXML
+    private Button Cancel;
+
+    @FXML
+    private Button Archive;
+
+    @FXML
+    private Button Save;
+
+    @FXML
+    private TextField Title;
+
+    @FXML
+    private TextField Module;
+
+    @FXML
+    private TextField NbMembers;
+
+    @FXML
+    private DatePicker FirstDay;
+
+    @FXML
+    private DatePicker LastDay;
+
+    @FXML
+    private TextArea Description;
+
+    public final UnaryOperator<TextFormatter.Change> integerOnlyFilter = change -> {
+        final String text = change.getText();
+        return (text.isEmpty() || text.matches("[0-9]*")) ? change : null;
+    };
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        final BooleanProperty firstTime = new SimpleBooleanProperty(true);
+
+        NbMembers.setTextFormatter(new TextFormatter(integerOnlyFilter));
+        Title.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue && firstTime.get()){
+                controllerView.requestFocus(); // Delegate the focus to container
+                firstTime.setValue(false); // Variable value changed for future references
+            }
+        });
+    }
+
+    @FXML
+    public void handleClickAddFile(ActionEvent event) {
+        log.debug("Add");
+    }
+
+    @FXML
+    public void handleClickCancel(ActionEvent event) throws IOException {
+        log.debug("Cancel");
+    }
+
+    @FXML
+    public void handleClickArchive(ActionEvent event) {
+        log.debug("Archive");
+    }
+
+    @FXML
+    public void handleClickSave(ActionEvent event) {
+        Alert alert;
+
+        ConfigurationView.setProject(Title.getText(), Module.getText(), NbMembers.getText(), FirstDay.getValue(), LastDay.getValue(), Description.getText());
+
+        if(!ConfigurationView.getProject().isComplete()) {
+            log.debug("Save");
+        } else {
+            alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information dialog");
+            alert.setHeaderText(null);
+            alert.setContentText("* fields must be completed");
+
+            alert.showAndWait();
+        }
+    }
 }
