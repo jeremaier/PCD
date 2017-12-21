1dd026c0ae033eff2aaf4cdbba9d599a70196d14
KARCZEWSKI Matta <matta.karczewski@telecomnancy.eu>
2017-12-22 00:29:36 +0100

    travail

--- src/main/java/eu/telecomnancy/pcd2k17/Main.java
+++ src/main/java/eu/telecomnancy/pcd2k17/Main.java
@@ -1,11 +1,13 @@
 package eu.telecomnancy.pcd2k17;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import javafx.application.Application;
 
 import javafx.stage.Stage;
 import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.models.Member;
 
 import java.util.Date;
 import java.util.List;
@@ -29,8 +31,13 @@ public class Main extends Application  {
   @Override
   public void start(Stage primaryStage) throws Exception {
 
-    new LogView(primaryStage);
+    //new LogView(primaryStage);
     gla = new GitLabApi("https://gitlab.telecomnancy.univ-lorraine.fr", "shcz2dxdWdAFw5dc7q6-");
+
+    List<org.gitlab4j.api.models.Member> list = gla.getProjectApi().getMembers(3196);
+    for (Member a: list){
+      System.out.println(a.getName());
+    }
       //new ProjectView("vQZL2K-1161fbxFAwLsS","Quent");
   }
 
